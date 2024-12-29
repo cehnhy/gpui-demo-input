42e42864d3264d65e8dd51bf8d0393135928033b
cehnhy <cehnhy@outlook.com>
2024-12-29 20:51:28 +0800

    feat: add some

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,13 @@ fn main() {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
                 ..Default::default()
             },
-            |cx| cx.new_view(|cx| Root::new(cx)),
+            |cx| {
+                cx.new_view(|cx| {
+                    let root = Root::new(cx);
+                    cx.focus_self();
+                    root
+                })
+            },
         )
         .unwrap();
 
