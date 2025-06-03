7edb8624a4d36527ce54ace1fdbbac0e9e80a560
cehnhy <cehnhy@outlook.com>
2025-06-03 09:57:15 +0800

    feat: update dep

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,7 @@
 use gpui::*;
-use gpui_component::input::{InputEvent, TextInput};
+use gpui_component::input::{InputEvent, InputState, TextInput};
 use prelude::FluentBuilder;
+use std::path::PathBuf;
 
 const CONTEXT: &str = "root";
 actions!(root, [Up, Down]);
@@ -13,7 +14,7 @@ pub fn init(cx: &mut App) {
 }
 
 pub struct Root {
-    query: Entity<TextInput>,
+    query: Entity<InputState>,
     state: Entity<State>,
     list_state: ListState,
     _update_list_task: Option<Task<()>>,
@@ -23,7 +24,7 @@ pub struct Root {
 impl Root {
     pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
         // query
-        let query = cx.new(|cx| TextInput::new(window, cx));
+        let query = cx.new(|cx| InputState::new(window, cx));
         cx.subscribe(&query, Self::on_input_event).detach();
 
         // state
@@ -54,7 +55,7 @@ impl Root {
     // on input event
     fn on_input_event(
         &mut self,
-        _query: Entity<TextInput>,
+        _query: Entity<InputState>,
         input_event: &InputEvent,
         cx: &mut Context<Self>,
     ) {
@@ -78,7 +79,7 @@ impl Root {
             state.reset();
 
             if let Ok(entries) = std::fs::read_dir("/Applications") {
-                let text_content = self.query.read(cx).text();
+                let text_content = self.query.read(cx).value();
                 for entry in entries {
                     if let Ok(entry) = entry {
                         let path = entry.path();
@@ -141,8 +142,15 @@ impl Render for Root {
             .size_full()
             .flex()
             .flex_col()
-            .child(div().m_2().child(self.query.clone()))
-            .child(list(self.list_state.clone()).w_full().h_full())
+            .bg(rgb(0x000000))
+            .child(div().p_2().child(TextInput::new(&self.query)))
+            .child(
+                div()
+                    .flex_1()
+                    .pb_2()
+                    .px_2()
+                    .child(list(self.list_state.clone()).size_full()),
+            )
     }
 }
 
@@ -219,16 +227,28 @@ impl RenderOnce for ListItem {
     fn render(self, _window: &mut Window, _cx: &mut App) -> impl IntoElement {
         div()
             .flex()
-            .flex_col()
+            .flex_row()
             .when(self.selected, |this| this.bg(rgb(0x2a2a2a)))
             .items_start()
-            .p_2()
-            .m_2()
+            .my_0p5()
             .rounded_md()
             .hover(|s| s.bg(rgb(0x3a3a3a)))
             .text_color(rgb(0xffffff))
             .text_xl()
-            .child(self.title.clone())
-            .child(div().flex().text_sm().child(self.subtitle.clone()))
+            .child(
+                img(PathBuf::from(
+                    "./app-icon.png", // TODO
+                ))
+                .h(px(56.0))
+                .w(px(56.0)),
+            )
+            .child(
+                div()
+                    .flex()
+                    .flex_col()
+                    .size_full()
+                    .child(self.title.clone())
+                    .child(div().flex().text_sm().child(self.subtitle.clone())),
+            )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
     Application::new().run(|cx: &mut App| {
         gpui_component::input::init(cx);
         gpui_component::theme::init(cx);
+        gpui_component::highlighter::init(cx);
         input::init(cx);
 
-        let bounds = Bounds::centered(None, size(px(600.0), px(318.0)), cx);
+        let bounds = Bounds::centered(None, size(px(600.0), px(295.0)), cx);
         cx.open_window(
             WindowOptions {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
@@ -18,9 +19,12 @@ fn main() {
             },
             |window, cx| {
                 cx.new(|cx| {
-                    let root = Root::new(window, cx);
-                    cx.focus_self(window);
-                    root
+                    let root = cx.new(|cx| {
+                        let root = Root::new(window, cx);
+                        cx.focus_self(window);
+                        root
+                    });
+                    gpui_component::Root::new(root.into(), window, cx)
                 })
             },
         )
