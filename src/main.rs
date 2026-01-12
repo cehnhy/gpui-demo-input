9dd58d60d3b0e0a0de10f11fd2b93a0b74783041
chy <cehnhy@outlook.com>
2026-01-12 11:38:30 +0800

    feat: linux

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,16 @@ fn main() {
                 cx.new(|cx| {
                     let root = cx.new(|cx| {
                         let root = Root::new(window, cx);
+                        cx.observe_window_activation(window, |_, window, _cx| {
+                            if !window.is_window_active() {
+                                window.remove_window();
+                            }
+                        })
+                        .detach();
                         cx.focus_self(window);
                         root
                     });
+                    cx.activate(true);
                     gpui_component::Root::new(root, window, cx)
                 })
             },
